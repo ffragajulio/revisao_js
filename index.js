7e6d4771bd9968eab8cd1ebab852919258822496
Julio Ribeiro <108158525+ffragajulio@users.noreply.github.com>
2024-03-08 11:00:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// comando para exibir mensagem
+console.log("O GREMIO VAI SAIR CAMPEÃO E O INTER NÃO!!!");
+
+// exercicio de calculos
+const num1 = 1903;
+const num2 = 654;
+
+// soma
+let soma = num1 + num2;
+console.log("o valor da soma é " + soma);
+
+// subtracao
+let subtracao = num1 - num2;
+console.log("o valor da subtracao é " + subtracao)
+// divisao
+let divisao = num1 / num2;
+let divisao_mod = num1 % num2;
+console.log("o valor da divisao é " + divisao);
+console.log("o valor do resto da divisao é " + divisao_mod);
+
+// multiplicacao
+let mult = num1 * num2
+console.log("o valor da multiplicacao é " + mult);
